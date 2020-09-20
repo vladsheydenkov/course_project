670dba45b5075c3a69264b602e8c2d7ca25625e3
vlad sheydenkov <vladsheydenkov@gmail.com>
2020-09-20 11:19:25 +0300

    rework admin panel

--- lesson/admin.py
+++ lesson/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 from . import models
 # Register your models here.
 
-admin.site.register(models.Material)
+# admin.site.register(models.Material)
+
+
+@admin.register(models.Material)
+class MaterialAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'material_type', 'publish')
+    list_filter = ('material_type', 'created')
+    search_fields = ('title', 'body')
+    prepopulated_fields = {'slug': ('title', )}
+    ordering = ('material_type', 'publish')
