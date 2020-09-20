57e9ca9a8ca12e907889a9670b497ad6949ff2a6
vlad sheydenkov <vladsheydenkov@gmail.com>
2020-09-20 17:31:28 +0300

    add all_views

--- lesson/views.py
+++ lesson/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from . import models
 # Create your views here.
+
+
+def all_materials(request):
+    materials = models.Material.objects.all()
+    return render(request,
+                  'materials/all_materials.html',
+                  {'materials': materials})
+
